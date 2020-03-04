a43e1db480c31493c1bf0b9423e5cd15e721749d
shihshihnihola <56904140+shihshihnihola@users.noreply.github.com>
2020-03-04 11:13:53 +0800

    lab01code-4

--- swanky_hango_gaaris1.ino
+++ swanky_hango_gaaris1.ino
@@ -7,6 +7,6 @@ void loop()
 {
   digitalWrite(9, HIGH);
   delay(2000); // Wait for 1000 millisecond(s)
-  digitalWrite(13, LOW);
+  digitalWrite(9, LOW);
   delay(500); // Wait for 1000 millisecond(s)
 }
