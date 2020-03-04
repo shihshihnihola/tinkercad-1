5e888fbc17ee199fd9bf35de7f29a339d9aa0f25
shihshihnihola <56904140+shihshihnihola@users.noreply.github.com>
2020-03-04 11:07:45 +0800

    Lab01-Second-3/4

--- swanky_hango_gaaris1.ino
+++ swanky_hango_gaaris1.ino
@@ -1,12 +1,12 @@
 void setup()
 {
-  pinMode(13, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(13, HIGH);
-  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(2000); // Wait for 1000 millisecond(s)
   digitalWrite(13, LOW);
-  delay(100); // Wait for 1000 millisecond(s)
+  delay(500); // Wait for 1000 millisecond(s)
 }
